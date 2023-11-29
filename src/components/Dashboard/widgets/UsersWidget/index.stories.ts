bd973f3d6a7bb8e494053ff2f7396478ca87610d
Gayun00 <gayunlee11@gmail.com>
2023-11-30 01:19:35 +0900

    Add: icon
    - 증감률에 따른 아이콘 추가

--- src/components/Dashboard/widgets/UsersWidget/index.stories.ts
+++ src/components/Dashboard/widgets/UsersWidget/index.stories.ts
@@ -13,7 +13,17 @@ const meta: Meta<typeof UsersWidget> = {
 export default meta;
 type Story = StoryObj<typeof UsersWidget>;
 
-export const UniqueEventWidget: Story = {
+export const ChangeRateIncreased: Story = {
+  args: {
+    title: "접속유저",
+    type: "SUM",
+    property: "Unique Event Count",
+    data: 18204,
+    changeRate: 93,
+  },
+};
+
+export const ChangeRateDecreased: Story = {
   args: {
     title: "접속유저",
     type: "SUM",
--- src/components/Dashboard/widgets/UsersWidget/index.tsx
+++ src/components/Dashboard/widgets/UsersWidget/index.tsx
@@ -7,6 +7,7 @@ import {
 } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { formatNumberWithCommas } from "@/utils/handleData";
+import { FaArrowUp, FaArrowDown } from "react-icons/fa";
 
 interface Props {
   title: string;
@@ -29,10 +30,19 @@ function UsersWidget({ title, type, property, data, changeRate }: Props) {
           <Badge variant="secondary">{type}</Badge>
           <p className="text-sm">{property}</p>
         </div>
-        <CardDescription className="text-4xl font-extrabold text-black">
-          {formatNumberWithCommas(data)}
+        <CardDescription className="space-y-3">
+          <p className="text-4xl font-extrabold text-black">
+            {formatNumberWithCommas(data)}
+          </p>
+          <div className="flex items-center gap-x-2">
+            {changeRate < 0 ? (
+              <FaArrowDown className="text-primary" />
+            ) : (
+              <FaArrowUp className="text-primary" />
+            )}
+            <p className="text-primary">{changeRate}</p>
+          </div>
         </CardDescription>
-        <p className="text-primary">{changeRate}</p>
       </CardContent>
     </Card>
   );
