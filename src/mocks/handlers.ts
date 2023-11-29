12ed56c320262cba6a86c020f772b324f9e15be9
Gayun00 <gayunlee11@gmail.com>
2023-11-30 00:18:21 +0900

    Chore: storybook 설정 추가

--- src/mocks/handlers.ts
+++
@@ -1,8 +0,0 @@
-import { http, HttpResponse } from "msw";
-import { users } from "./data/users";
-
-export const handlers = [
-  http.get("/ex", () => {
-    return HttpResponse.json(users);
-  }),
-];
