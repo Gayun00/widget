ce5caffa97e328d6b37f15436c6bcd8d9bde9d85
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-30 01:56:50 +0900

    Add: 접속 유저, 접속 횟수 총합 위젯 구현 (#8)
    * Chore: 폴더명 컴포넌트명에 맞게 변경
    
    * Add: UniqueEventCountSum
    
    * Chore: UniqueEventCountSum 폴더 이동
    
    * Add: UniqueEventCountSum, TotalEventCountSum

---
+++ src/components/Dashboard/widgets/TotalEventCountSum/index.tsx
@@ -0,0 +1,27 @@
+import { useMemo } from "react";
+import StatsOverViewWidget from "../StatsOverViewWidget";
+import { useUsersQuery } from "@/queries";
+import { getTotalEventSumByDate } from "@/utils/handleData";
+
+function TotalEventCountSum() {
+  const { data: userEvents } = useUsersQuery();
+  // TODO: 날짜 선택 기능 추가 후 교체. 현재는 데이터 처리 가능한 날짜로 임시 지정
+  // const date = dayjs().format("YYYY-MM-DD");
+  const date = "2022-03-23";
+  console.log(userEvents);
+  const data: number = useMemo(() => {
+    if (!userEvents?.data?.rows) throw new Error("Data not found");
+    return getTotalEventSumByDate(userEvents.data.rows, date);
+  }, []);
+  return (
+    <StatsOverViewWidget
+      title="접속유저"
+      type="SUM"
+      property="Unique Event Count"
+      data={data}
+      changeRate={93}
+    />
+  );
+}
+
+export default TotalEventCountSum;
---
+++ src/components/Dashboard/widgets/UniqueEventCountSum/index.tsx
@@ -0,0 +1,29 @@
+import { useMemo } from "react";
+import StatsOverViewWidget from "../StatsOverViewWidget";
+import { useUsersQuery } from "@/queries";
+import { getUniqueEventSumByDate } from "@/utils/handleData";
+// import dayjs from "dayjs";
+
+function UniqueEventCountSum() {
+  const { data: userEvents } = useUsersQuery();
+  // TODO: 날짜 선택 기능 추가 후 교체. 현재는 데이터 처리 가능한 날짜로 임시 지정
+  // const date = dayjs().format("YYYY-MM-DD");
+  console.log(userEvents);
+  const date = "2022-03-23";
+  const data: number = useMemo(() => {
+    if (!userEvents?.data?.rows) throw new Error("Data not found");
+    return getUniqueEventSumByDate(userEvents.data.rows, date);
+  }, [userEvents, date]);
+
+  return (
+    <StatsOverViewWidget
+      title="접속유저"
+      type="SUM"
+      property="Unique Event Count"
+      data={data}
+      changeRate={93}
+    />
+  );
+}
+
+export default UniqueEventCountSum;
