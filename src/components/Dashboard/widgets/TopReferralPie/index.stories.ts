a9ed73c63748af62037f9ce4c11b0d8a84771460
Gayun00 <gayunlee11@gmail.com>
2023-11-30 11:53:02 +0900

    Add: TopReferralPie 컴포넌트

---
+++ src/components/Dashboard/widgets/TopReferralPie/index.stories.ts
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Component from "../TopReferralPie/View";
+
+const meta: Meta<typeof Component> = {
+  title: "widgets/TopReferralPie",
+  component: Component,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "fullscreen",
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Component>;
+
+export const Default: Story = {
+  args: {
+    data: [44, 55, 13, 43, 22],
+    labels: ["Team A", "Team B", "Team C", "Team D", "Team E"],
+  },
+};
---
+++ src/components/Dashboard/widgets/TopReferralPie/index.tsx
@@ -0,0 +1,13 @@
+import View from "./View";
+
+function TopReferralPie() {
+  // TODO: api 응답값으로 교체
+  return (
+    <View
+      data={[44, 55, 13, 43, 22]}
+      labels={["Team A", "Team B", "Team C", "Team D", "Team E"]}
+    />
+  );
+}
+
+export default TopReferralPie;
