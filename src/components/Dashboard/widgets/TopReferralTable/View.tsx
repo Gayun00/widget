6ffdc0740810c7a2082a72e0ede5883819a1fd5e
Gayun00 <gayunlee11@gmail.com>
2023-11-30 21:18:27 +0900

    Add: tanstack/react-table 사용해 그룹핑 구현

--- src/components/Dashboard/widgets/TopReferralTable/View.tsx
+++ src/components/Dashboard/widgets/TopReferralTable/View.tsx
@@ -1,93 +1,233 @@
 import React from "react";
+
 import {
+  GroupingState,
   useReactTable,
+  getPaginationRowModel,
+  getFilteredRowModel,
   getCoreRowModel,
   getGroupedRowModel,
-  flexRender,
+  getExpandedRowModel,
   ColumnDef,
+  flexRender,
 } from "@tanstack/react-table";
-
-interface RowData {
+interface Person {
   country: string;
   region: string;
   city: string;
   uniqueEventCount: string;
 }
-
-const convertDataToObjects = (dataArray) =>
-  dataArray.map(([country, region, city, uniqueEventCount]) => ({
-    country,
-    region,
-    city,
-    uniqueEventCount,
-  }));
-
-const columns: ColumnDef<RowData>[] = [
+const initialData = [
   {
-    accessorKey: "country",
-    header: "Country",
+    country: "USA",
+    region: "California",
+    city: "newyork",
+    uniqueEventCount: "119",
   },
   {
-    accessorKey: "region",
-    header: "Region",
+    country: "USA",
+    region: "California",
+    city: "newyork",
+    uniqueEventCount: "119",
   },
   {
-    accessorKey: "city",
-    header: "City",
-  },
-  {
-    accessorKey: "uniqueEventCount",
-    header: "Unique Event Count",
+    country: "USA",
+    region: "California",
+    city: "newyork",
+    uniqueEventCount: "119",
   },
 ];
 
-interface Props {
-  data: string[][];
-}
+export default function View() {
+  const columns = React.useMemo<ColumnDef<Person>[]>(
+    () => [
+      {
+        accessorKey: "country",
+        header: "Country",
+        cell: (info) => info.getValue(),
+        /**
+         * override the value used for row grouping
+         * (otherwise, defaults to the value derived from accessorKey / accessorFn)
+         */
+      },
+      {
+        accessorFn: (row) => row.region,
+        id: "region",
+        header: () => <span>Region</span>,
+        cell: (info) => info.getValue(),
+      },
+      {
+        accessorKey: "city",
+        header: () => "City",
+        cell: (info) => info.getValue(),
+      },
+      {
+        accessorKey: "uniqueEventCount",
+        header: () => "Unique Event Count",
+        cell: (info) => Number(info.getValue()),
+        aggregationFn: "sum",
+        aggregatedCell: ({ getValue }) => Number(getValue()) || "",
+      },
+    ],
+    []
+  );
 
-const View = ({ data }: Props) => {
-  const dataObjects = React.useMemo(() => convertDataToObjects(data), [data]);
+  const [data] = React.useState(initialData);
+
+  const [grouping, setGrouping] = React.useState<GroupingState>([
+    // "country",
+    // "region",
+    // "city",
+  ]);
 
   const table = useReactTable({
-    data: dataObjects,
+    data,
     columns,
-    getCoreRowModel: getCoreRowModel(),
+    state: {
+      grouping,
+    },
+    onGroupingChange: setGrouping,
+    getExpandedRowModel: getExpandedRowModel(),
     getGroupedRowModel: getGroupedRowModel(),
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    debugTable: true,
   });
 
   return (
-    <table>
-      <thead>
-        {table.getHeaderGroups().map((headerGroup) => (
-          <tr key={headerGroup.id}>
-            {headerGroup.headers.map((header) => (
-              <th key={header.id}>
-                {header.isPlaceholder ? null : (
-                  <div>
-                    {flexRender(
-                      header.column.columnDef.header,
-                      header.getContext()
+    <div className="p-2">
+      <div className="h-2" />
+      <table>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <th key={header.id} colSpan={header.colSpan}>
+                    {header.isPlaceholder ? null : (
+                      <div>
+                        {header.column.getCanGroup() ? (
+                          // If the header can be grouped, let's add a toggle
+                          <button
+                            {...{
+                              onClick: header.column.getToggleGroupingHandler(),
+                              style: {
+                                cursor: "pointer",
+                              },
+                            }}>
+                            {header.column.getIsGrouped()
+                              ? `🛑(${header.column.getGroupedIndex()}) `
+                              : `👊 `}
+                          </button>
+                        ) : null}{" "}
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                      </div>
                     )}
-                  </div>
-                )}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody>
-        {table.getRowModel().rows.map((row) => (
-          <tr key={row.id}>
-            {row.getVisibleCells().map((cell) => (
-              <td key={cell.id}>
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-};
+                  </th>
+                );
+              })}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => {
+            return (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => {
+                  return (
+                    <td
+                      {...{
+                        key: cell.id,
+                        style: {
+                          background: cell.getIsGrouped()
+                            ? "#0aff0082"
+                            : cell.getIsAggregated()
+                            ? "#ffa50078"
+                            : cell.getIsPlaceholder()
+                            ? "#ff000042"
+                            : "white",
+                        },
+                      }}>
+                      {cell.getIsGrouped() ? (
+                        // If it's a grouped cell, add an expander and row count
+                        <>
+                          <button
+                            {...{
+                              onClick: row.getToggleExpandedHandler(),
+                              style: {
+                                cursor: row.getCanExpand()
+                                  ? "pointer"
+                                  : "normal",
+                              },
+                            }}>
+                            {row.getIsExpanded() ? "👇" : "👉"}{" "}
+                            {flexRender(
+                              cell.column.columnDef.cell,
+                              cell.getContext()
+                            )}{" "}
+                            ({row.subRows.length})
+                          </button>
+                        </>
+                      ) : cell.getIsAggregated() ? (
+                        // If the cell is aggregated, use the Aggregated
+                        // renderer for cell
+                        flexRender(
+                          cell.column.columnDef.aggregatedCell ??
+                            cell.column.columnDef.cell,
+                          cell.getContext()
+                        )
+                      ) : cell.getIsPlaceholder() ? null : ( // For cells with repeated values, render null
+                        // Otherwise, just render the regular cell
+                        flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )
+                      )}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div className="h-2" />
+      <div className="flex items-center gap-2">
+        <button
+          className="border rounded p-1"
+          onClick={() => table.setPageIndex(0)}
+          disabled={!table.getCanPreviousPage()}>
+          {"<<"}
+        </button>
+        <button
+          className="border rounded p-1"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}>
+          {"<"}
+        </button>
+        <button
+          className="border rounded p-1"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}>
+          {">"}
+        </button>
+        <button
+          className="border rounded p-1"
+          onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+          disabled={!table.getCanNextPage()}>
+          {">>"}
+        </button>
+        <span className="flex items-center gap-1">
+          <div>Page</div>
+        </span>
+      </div>
+      <div>{table.getRowModel().rows.length} Rows</div>
 
-export default View;
+      <pre>{JSON.stringify(grouping, null, 2)}</pre>
+    </div>
+  );
+}
