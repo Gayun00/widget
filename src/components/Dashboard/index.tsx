be589e1f893d4bc9b05a9962c363b933c35d3782
gygy <67543454+Gayun00@users.noreply.github.com>
2023-12-01 19:55:51 +0900

    Add: 레이아웃 초기화, 위젯 고정 버튼 추가 (#19)

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -7,6 +7,8 @@ import DAU from "./widgets/DAU";
 import TopReferralPie from "./widgets/TopReferralPie";
 import TopReferralTable from "./widgets/TopReferralTable";
 import "./index.css";
+import { Toggle } from "../ui/toggle";
+import { Button } from "../ui/button";
 
 const ReactGridLayout = WidthProvider(RGL);
 
@@ -37,6 +39,7 @@ const Dashboard = ({ className, rowHeight, cols }: Props) => {
   const [layout, setLayout] = useState(
     savedLayout ? JSON.parse(savedLayout) : initialLayout
   );
+  const [isDraggable, setIsDraggable] = useState(true);
 
   const handleLayoutChange = (newLayout: Layout[]) => {
     setLayout(newLayout);
@@ -44,6 +47,14 @@ const Dashboard = ({ className, rowHeight, cols }: Props) => {
       STORAGE_KEY.DASHBOARD_LAYOUT_1,
       JSON.stringify(layout)
     );
+  };
+
+  const initializeLayout = () => {
+    setLayout(initialLayout);
+  };
+
+  const handleDraggable = () => {
+    setIsDraggable(!isDraggable);
   };
 
   useEffect(() => {
@@ -54,28 +65,38 @@ const Dashboard = ({ className, rowHeight, cols }: Props) => {
   }, [layout]);
 
   return (
-    <ReactGridLayout
-      className={className || "layout"}
-      layout={layout}
-      onLayoutChange={handleLayoutChange}
-      rowHeight={rowHeight}
-      cols={cols}>
-      <div key="0">
-        <UniqueEventCountSum />
+    <div>
+      <div className="py-3 flex justify-center gap-x-5">
+        <Toggle onClick={handleDraggable}>위젯 고정</Toggle>
+        <Button variant="outline" onClick={initializeLayout}>
+          레이아웃 초기화
+        </Button>
       </div>
-      <div key="1">
-        <TotalEventCountSum />
-      </div>
-      <div key="2">
-        <DAU startDate="2022-03-23" endDate="2022-03-28" />
-      </div>
-      <div key="3">
-        <TopReferralPie />
-      </div>
-      <div key="4">
-        <TopReferralTable />
-      </div>
-    </ReactGridLayout>
+
+      <ReactGridLayout
+        className={className || "layout"}
+        isDraggable={isDraggable}
+        layout={layout}
+        onLayoutChange={handleLayoutChange}
+        rowHeight={rowHeight}
+        cols={cols}>
+        <div key="0">
+          <UniqueEventCountSum />
+        </div>
+        <div key="1">
+          <TotalEventCountSum />
+        </div>
+        <div key="2">
+          <DAU startDate="2022-03-23" endDate="2022-03-28" />
+        </div>
+        <div key="3">
+          <TopReferralPie />
+        </div>
+        <div key="4">
+          <TopReferralTable />
+        </div>
+      </ReactGridLayout>
+    </div>
   );
 };
 
