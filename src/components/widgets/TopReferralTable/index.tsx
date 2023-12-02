ae78af60f3e86ed3b1d7632daf97a5ceee786753
Gayun00 <gayunlee11@gmail.com>
2023-12-02 19:56:18 +0900

    Fix: columns 선언 방식 변경

--- src/components/widgets/TopReferralTable/index.tsx
+++ src/components/widgets/TopReferralTable/index.tsx
@@ -1,45 +1,42 @@
-import React, { useMemo } from "react";
+import { useMemo } from "react";
 import { convertArrayToObject } from "@/utils/handleData";
 import View from "./View";
 import { useTopReferralAreaQuery } from "@/queries";
 import { ColumnDef } from "@tanstack/react-table";
 import { TopReferralTableData } from "@/types/api";
 
-const columns = React.useMemo<ColumnDef<TopReferralTableData>[]>(
-  () => [
-    {
-      accessorKey: "country",
-      header: "Country",
-      cell: (info) => info.getValue(),
-      enableSorting: true,
+const columns: ColumnDef<TopReferralTableData>[] = [
+  {
+    accessorKey: "country",
+    header: "Country",
+    cell: (info) => info.getValue(),
+    enableSorting: true,
+  },
+  {
+    accessorKey: "region",
+    id: "region",
+    header: () => <span>Region</span>,
+    cell: (info) => info.getValue(),
+  },
+  {
+    accessorKey: "city",
+    header: () => <span>City</span>,
+    cell: (info) => info.getValue(),
+  },
+  {
+    accessorKey: "uniqueEventCount",
+    header: "Unique event count",
+    cell: (info) => info.getValue(),
+    aggregationFn: (_, leafRows) => {
+      const total = leafRows.reduce((sum, row) => {
+        const value = parseInt(row.getValue("uniqueEventCount"));
+        return sum + value;
+      }, 0);
+      return total;
     },
-    {
-      accessorKey: "region",
-      id: "region",
-      header: () => <span>Region</span>,
-      cell: (info) => info.getValue(),
-    },
-    {
-      accessorKey: "city",
-      header: () => <span>City</span>,
-      cell: (info) => info.getValue(),
-    },
-    {
-      accessorKey: "uniqueEventCount",
-      header: "Unique event count",
-      cell: (info) => info.getValue(),
-      aggregationFn: (_, leafRows) => {
-        const total = leafRows.reduce((sum, row) => {
-          const value = parseInt(row.getValue("uniqueEventCount"));
-          return sum + value;
-        }, 0);
-        return total;
-      },
-      aggregatedCell: ({ getValue }) => getValue(),
-    },
-  ],
-  []
-);
+    aggregatedCell: ({ getValue }) => getValue(),
+  },
+];
 function TopReferralTable() {
   const { data: topReferral } = useTopReferralAreaQuery();
   const data = useMemo(() => {
--- src/components/widgets/WidgetLayout/index.tsx
+++ src/components/widgets/WidgetLayout/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import { ReactNode } from "react";
 import EmptyWidgetData from "@/components/fallbacks/EmptyWidgetData";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
