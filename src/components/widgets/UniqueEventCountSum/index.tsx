8a26df5b25278fb1873968f4fc6c088c9482084f
gygy <67543454+Gayun00@users.noreply.github.com>
2023-12-02 20:59:09 +0900

    Correct: 오늘 날짜 props 임의 지정 (#27)

--- src/components/widgets/UniqueEventCountSum/index.tsx
+++ src/components/widgets/UniqueEventCountSum/index.tsx
@@ -2,14 +2,14 @@ import { useMemo } from "react";
 import { getUniqueEventSumByDate } from "@/utils/handleData";
 import StatsOverViewWidget from "@/components/widgets/StatsOverViewWidget";
 import { useUserEventsQuery } from "@/queries";
-// import dayjs from "dayjs";
 
-function UniqueEventCountSum() {
+interface Props {
+  date: string;
+}
+
+function UniqueEventCountSum({ date }: Props) {
   const { data: userEvents } = useUserEventsQuery();
 
-  // TODO: 날짜 선택 기능 추가 후 교체. 현재는 데이터 처리 가능한 날짜로 임시 지정
-  // const date = dayjs().format("YYYY-MM-DD");
-  const date = "2022-03-23";
   const data: number | undefined = useMemo(() => {
     if (!userEvents) return;
     return getUniqueEventSumByDate(userEvents, date);
